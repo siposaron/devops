5e2687080bc0b8a97a57cfc7a1993b779fd5c51a
sipos.aron <sipos.aron@codespring.ro>
2020-08-12 16:40:35 +0300

    jenkins docker agent

---
+++ docker/jenkins/start-agent.sh
@@ -0,0 +1,13 @@
+# Starts a Jenkins agent in docker container. env.conf file is needed besides this shell script.
+FILE=env.conf
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist, create it besides start-agent.sh with Jenkins variables: 
+        JENKINS_SECRET=
+        JENKINS_TUNNEL=
+        JENKINS_AGENT_NAME=
+        JENKINS_NAME=
+        JENKINS_AGENT_WORKDIR=
+        JENKINS_URL="
+    exit -1;
+fi
+docker run --env-file env.conf --init jenkins/jnlp-slave
